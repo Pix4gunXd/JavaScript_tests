68a245b63ee77ce1b22282c2044645d8e92342b2
Pix4gunXd <Caio.pompei@gmail.com>
2024-05-02 18:40:04 -0300

    Add File

---
+++ Words_Breaker.js
@@ -0,0 +1,20 @@
+var str = "Write some words here and test the code";
+
+var done = false;
+
+for (i = 0; i <= str.length; i++){
+
+    if(str.charAt(i - 1) == ' '){ //Separates the words
+        done = false;
+    }
+
+    if(done == false){
+       for (j = i+1; j <= str.length; j++){
+            if (str.charAt(j) == ' ' || j == str.length) {
+                console.log(str.slice(i,j));
+                done = true;
+                break;
+            }
+       }
+    }
+}
